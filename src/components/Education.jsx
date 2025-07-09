905b2fb0d03ea4c9f41c92688ac544253637ac0d
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-08 23:03:59 -0400

    add Education component

---
+++ src/components/Education.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Input from "./Input";
+import EducationList from "./EducationList";
+import AddEducationItem from "./AddEducationItem";
+
+let nextId = 0;
+export default function Education() {
+  const [items, setItems] = useState([]);
+  const [isEditing, setIsEditing] = useState(true);
+
+  function handleAddItem(schoolName, areaOfStudy, graduationDate) {
+    setItems([
+      ...items,
+      {
+        id: nextId++,
+        schoolName: schoolName,
+        areaOfStudy: areaOfStudy,
+        graduationDate: graduationDate,
+      },
+    ]);
+  }
+
+  function handleDeleteItem(itemId) {
+    setItems(items.filter((item) => item.id !== itemId));
+  }
+
+  function handleClick() {
+    setIsEditing(!isEditing);
+  }
+
+  return (
+    <>
+      <div className="header-row">
+        <h2>Education</h2>
+        <button onClick={handleClick}>{isEditing ? "Submit" : "Edit"}</button>
+      </div>
+      {isEditing ? (
+        <div>
+          <AddEducationItem onAddItem={handleAddItem} />
+          <EducationList items={items} onDeleteItem={handleDeleteItem} />
+        </div>
+      ) : (
+        <EducationList items={items} onDeleteItem={handleDeleteItem} />
+      )}
+    </>
+  );
+}
